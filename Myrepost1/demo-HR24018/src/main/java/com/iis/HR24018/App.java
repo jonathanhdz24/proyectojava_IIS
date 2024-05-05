8144dca31c64d0520b171f827611cf19ad818183
jonathanhdz24 <114651788+jonathanhdz24@users.noreply.github.com>
2024-05-05 04:10:55 -0600

    Add files via upload

---
+++ Myrepost1/demo-HR24018/src/main/java/com/iis/HR24018/App.java
@@ -0,0 +1,17 @@
+package com.iis.HR24018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
